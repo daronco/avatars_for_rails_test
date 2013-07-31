40644768c5d59e70a78ae6cecd921272781d4918
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2013-07-30 22:31:09 -0300

    Fixes to allow params in Member to be set
    And show more logos in members/show.

--- app/models/member.rb
+++ app/models/member.rb
@@ -5,5 +5,6 @@ class Member < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   acts_as_avatarable
-  attr_accessible :email, :name, :password, :password_confirmation
+  attr_accessible :email, :name, :password, :password_confirmation, :logo,
+    :logo_crop_x, :logo_crop_y, :logo_crop_w, :avatar_tmp_basename
 end
